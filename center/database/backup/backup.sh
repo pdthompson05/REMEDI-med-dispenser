7a3df0cac14545514f4317726f10517539ddc753
yinglijr <yinglijr@dukes.jmu.edu>
2025-02-19 21:01:05 -0500

    Cronjob for weekly backups created, backups folder, backup.sh for mysqldump, credentials from .env for security

--- center/database/backup/backup.sh
+++ center/database/backup/backup.sh
@@ -1,7 +1,7 @@
 # Cronjob created to run this weekly on sunday at 2am
 # crontab -e
 # Load environment variables from .env
-export $(grep -v '^#' ../../.env | xargs)
+export $(grep -v '^#' /var/www/it313communityprojects.website/section-three/.env | xargs)
 
 # Run mysqldump using credentials from .env
 mysqldump -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > "/var/www/it313communityprojects/section-three/center/database/backup/db_remidi_backup_$(date +%F).sql"
