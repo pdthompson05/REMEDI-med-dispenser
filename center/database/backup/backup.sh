505eab35beffd383c120bdb6fcaa9702d4ed9b0c
yinglijr <yinglijr@dukes.jmu.edu>
2025-02-19 21:06:53 -0500

    backup.sh logic fix

--- center/database/backup/backup.sh
+++ center/database/backup/backup.sh
@@ -1,10 +1,20 @@
 # Cronjob created to run this weekly on sunday at 2am
 # crontab -e
-# Load environment variables from .env
-export $(grep -v '^#' /var/www/it313communityprojects.website/section-three/.env | xargs)
+
+ENV_PATH="/var/www/it313communityprojects.website/section-three/.env"
+# Load environment variables
+export $(grep -v '^#' $ENV_PATH | xargs)
+
+BACKUP_DIR="/var/www/it313communityprojects.website/section-three/center/database/backup"
+mkdir -p "$BACKUP_DIR"
+BACKUP_FILE="$BACKUP_DIR/db_remidi_backup_$(date +%F).sql"
 
 # Run mysqldump using credentials from .env
-mysqldump -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > "/var/www/it313communityprojects/section-three/center/database/backup/db_remidi_backup_$(date +%F).sql"
+mysqldump -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > "$BACKUP_FILE"
 
-# Compress file
-gzip "/var/www/it313communityprojects/section-three/center/database/backup/db_remidi_backup_$(date +%F).sql"
+if [[ -f "$BACKUP_FILE" ]]; then
+    gzip "$BACKUP_FILE"
+else
+    echo "Error: Backup failed."
+    exit 1
+fi
