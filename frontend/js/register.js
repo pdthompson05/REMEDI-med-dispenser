1c126bad5d2bab7925b5e1a1b6964800d673a578
Jason <jasonamaya6@icloud.com>
2025-02-28 01:20:37 +0000

    register test

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -3,10 +3,8 @@ function createAccount() {
     let last_name = document.getElementById('last-name').value.trim();
     let email = document.getElementById('email').value.trim();
     let password = document.getElementById('password').value.trim();
-    let dob = document.getElementById('dob').value.trim();
-    let account_type = document.getElementById('account-type').value.trim();
 
-    if (!email || !password || !first_name || !last_name || !dob || !account_type) {
+    if (!email || !password || !first_name || !last_name) {
         document.getElementById('message').textContent = "All fields are required";
         return;
     }
@@ -27,8 +25,6 @@ function createAccount() {
     formData.append('last_name', last_name);
     formData.append('email', email);
     formData.append('password', password);
-    formData.append('dob', dob);
-    formData.append('account_type', account_type);
 
     fetch('/var/www/it313communityprojects/section-three/center/php/register.php', {
         method: 'POST',
@@ -44,6 +40,6 @@ function createAccount() {
         })
         .catch(error => {
             console.error('Error:', error);
-            document.getElementById('message').textContent = "Registration fail";
+            document.getElementById('message').textContent = "Registration failure";
         });
 }
