721868b28f0d4fc242fe054981a7be297197628a
jasonamaya6 <jasonamaya6@icloud.com>
2025-03-27 16:22:53 -0400

    fixing login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -25,7 +25,7 @@ function login() {
             console.log("Raw Response:", jsonData);
             document.getElementById('message').textContent = jsonData.message;
             if (jsonData.status === "success") {
-                window.location.href = 'profile.html';
+                window.location.href = 'index.html';
             }
         })
         .catch(error => {
