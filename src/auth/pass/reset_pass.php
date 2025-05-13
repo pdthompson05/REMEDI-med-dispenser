d3c8f5354b740fffde3703488e33c9fbc4eed5f0
yinglijr <yinglijr@dukes.jmu.edu>
2025-05-12 22:59:36 -0400

    style

--- src/auth/pass/reset_pass.php
+++ src/auth/pass/reset_pass.php
@@ -8,8 +8,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Reset Password | REMEDI</title>
-  <link rel="stylesheet" href="../../frontend/html/css/global.css" />
-  <link rel="stylesheet" href="../../frontend/html/css/login.css" />
+  <link rel="stylesheet" href="../../../frontend/html/css/global.css" />
+  <link rel="stylesheet" href="../../../frontend/html/css/login.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
 </head>
 <body class="login-page">
