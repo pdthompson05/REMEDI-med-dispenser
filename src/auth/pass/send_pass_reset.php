f0defb38dbdebbce675b54d179bc1bceb484b86c
yinglijr <yinglijr@dukes.jmu.edu>
2025-05-13 11:45:08 -0400

    pass reset fix

--- src/auth/pass/send_pass_reset.php
+++ src/auth/pass/send_pass_reset.php
@@ -3,7 +3,7 @@
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require_once __DIR__.'/../../../config/db.php';
+require_once __DIR__.'/../../config/db.php';
 require_once 'mail.php';
 global $conn;
 
